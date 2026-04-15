643075acf2ce6d49c28edbf1399f150cc78fdb30
Courtney B <courtney.brown5405@gmail.com>
2026-04-15 17:15:56 -0400

    Initial implementation of terminate

--- src/student.c
+++ src/student.c
@@ -123,7 +123,10 @@ extern void yield(unsigned int cpu_id)
  */
 extern void terminate(unsigned int cpu_id)
 {
-    /* FIX ME */
+   pthread_mutex_lock(&current_mutex);
+   current[cpu_id]->state = PROCESS_TERMINATED;
+   pthread_mutex_unlock(&current_mutex);
+   schedule(cpu_id);
 }
 
 
